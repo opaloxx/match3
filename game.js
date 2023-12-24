2533c095c9405e9510da806b59e57d4a8b9fad0c
Lisa Gudz <lizagudzz@gmail.com>
2023-12-24 21:56:16 +0300

    Improve logic and visualisation

--- game.js
+++ game.js
@@ -5,16 +5,19 @@ const SCREEN_COLOR = 'rgb(0, 0, 0)'; // Используйте строку дл
 const SELECT_COLOR = 'rgb(255, 255, 255)'; // Используйте строку для цвета
 const SELECT_WIDTH = BLOCK_SIZE / 10;
 const INACTIVE_COLOR_COEF = 0.5;
+const FALL_SLOWNESS = 25;
+const ANIMATION_INTERVAL = 10;
 
 // Цвета кружочков
 const circleColors = [
-    'rgb(255, 0, 0)',
-    'rgb(0, 255, 0)',
-    'rgb(0, 0, 255)',
-    'rgb(255, 255, 0)',
-    'rgb(255, 0, 255)',
-    'rgb(0, 255, 255)'
+    'rgb(228,113,122)',
+    'rgb(168,228,160)',
+    'rgb(95,158,160)',
+    'rgb(239,169,74)',
+    'rgb(250,218,221)',
+    'rgb(154,206,235)'
 ];
+
 
 function fillCanvas(color) {
     // Получение элемента canvas и его контекста
@@ -28,6 +31,7 @@ function fillCanvas(color) {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+
 function getMousePos(canvas, event) {
     let rect = canvas.getBoundingClientRect();
     return {
@@ -36,13 +40,15 @@ function getMousePos(canvas, event) {
     };
 }
 
+
 function mouseToBoard(mousePos) {
     let x = Math.floor(mousePos.x / BLOCK_SIZE);
     let y = Math.floor(mousePos.y / BLOCK_SIZE);
     return { x, y };
 }
 
-function drawBoard(ctx, board, pos, selected, active) {
+
+function drawBoard(ctx, board, pos, selected, active, shift) {
     // Заполнение холста основным цветом
     fillCanvas(SCREEN_COLOR);
     //ctx.fillStyle = SCREEN_COLOR;
@@ -63,7 +69,7 @@ function drawBoard(ctx, board, pos, selected, active) {
             ctx.beginPath();
             ctx.arc(
                 i * BLOCK_SIZE + BLOCK_SIZE / 2,
-                j * BLOCK_SIZE + BLOCK_SIZE / 2,
+                j * BLOCK_SIZE + BLOCK_SIZE / 2 + BLOCK_SIZE * shift[i][j] / FALL_SLOWNESS,
                 BLOCK_SIZE / 2,
                 0,
                 Math.PI * 2
@@ -93,7 +99,6 @@ function drawBoard(ctx, board, pos, selected, active) {
 }
 
 
-
 function changeColors(board) {
     for (let i = 0; i < BOARD_SIZE; i++) {
         for (let j = 0; j < BOARD_SIZE; j++) {
@@ -104,59 +109,117 @@ function changeColors(board) {
     }
 }
 
-function match3(board, active) {
-    let changed = false;
 
-    // Проверка по горизонтали
+function isInBoard(x, y) {
+    return 0 <= x && x < BOARD_SIZE && 0 <= y && y < BOARD_SIZE;
+}
+
+
+function checkEqualItems(x1, y1, x2, y2, board, active) {
+    if (!isInBoard(x1, y1) || !isInBoard(x2, y2)) {
+        return false;
+    } 
+    if (board[x1][y1] === null || board[x2][y2] === null) {
+        return false;
+    }
+    if (!active[x1][y1] || !active[x2][y2]) {
+        return false;
+    }
+    return board[x1][y1] === board[x2][y2];
+}
+
+
+function match3Horizontal(board, boardCopy, active) {
+    let changed = false;
     for (let i = 0; i < BOARD_SIZE; i++) {
         let cnt = 1;
-        for (let j = 1; j < BOARD_SIZE; j++) {
-            if (board[i][j] === board[i][j - 1] && board[i][j] !== null && active[i][j] && active[i][j - 1]) {
+        for (let j = 1; j < BOARD_SIZE + 1; j++) {
+            if (checkEqualItems(i, j, i, j - 1, board, active)) {
                 cnt++;
                 continue;
             }
             if (cnt >= 3) {
                 changed = true;
                 for (let k = j - cnt; k < j; k++) {
-                    board[i][k] = null;
+                    boardCopy[i][k] = null;
                 }
             }
             cnt = 1;
         }
-        if (cnt >= 3) {
-            changed = true;
-            for (let k = BOARD_SIZE - cnt; k < BOARD_SIZE; k++) {
-                board[i][k] = null;
-            }
-        }
     }
+    return changed;
+} 
 
-    // Проверка по вертикали
+
+function match3Vertical(board, boardCopy, active) {
+    let changed = false;
     for (let j = 0; j < BOARD_SIZE; j++) {
         let cnt = 1;
-        for (let i = 1; i < BOARD_SIZE; i++) {
-            if (board[i][j] === board[i - 1][j] && board[i][j] !== null && active[i][j] && active[i - 1][j]) {
+        for (let i = 1; i < BOARD_SIZE + 1; i++) {
+            if (checkEqualItems(i, j, i - 1, j, board, active)) {
                 cnt++;
                 continue;
             }
             if (cnt >= 3) {
                 changed = true;
                 for (let k = i - cnt; k < i; k++) {
-                    board[k][j] = null;
+                    boardCopy[k][j] = null;
                 }
             }
             cnt = 1;
         }
-        if (cnt >= 3) {
-            changed = true;
-            for (let k = BOARD_SIZE - cnt; k < BOARD_SIZE; k++) {
-                board[k][j] = null;
+    }
+    return changed;
+}
+
+
+function match3Squares(board, boardCopy, active) {
+    let changed = false;
+    for (let i = 0; i < BOARD_SIZE - 1; i++) {
+        for (let j = 1; j < BOARD_SIZE - 1; j++) {
+            if (board[i][j] === null) {
+                continue;
             }
+            if (!active[i][j] || !active[i + 1][j] || !active[i][j + 1] || !active[i + 1][j + 1]) {
+                continue;
+            }
+            if (board[i][j] != board[i + 1][j] || board[i][j] != board[i][j + 1] || board[i][j] != board[i + 1][j + 1]) {
+                continue;
+            }
+            boardCopy[i][j] = null;
+            boardCopy[i + 1][j] = null;
+            boardCopy[i][j + 1] = null;
+            boardCopy[i + 1][j + 1] = null;
+            changed = true;
+            console.log(board[i][j], board[i + 1][j], board[i][j + 1], board[i + 1][j + 1]);
+        }
+    }
+    return changed;
+}
+
+
+function match3(board, active) {
+    let changed = false;
+    let boardCopy = [];
+    for (let i = 0; i < BOARD_SIZE; i++) {
+        boardCopy.push(new Array(BOARD_SIZE).fill(0));
+        for (let j = 0; j < BOARD_SIZE; j++) {
+            boardCopy[i][j] = board[i][j];
         }
     }
 
+    changed |= match3Horizontal(board, boardCopy, active);
+    changed |= match3Vertical(board, boardCopy, active);
+    changed |= match3Squares(board, boardCopy, active);
+
+    for (let i = 0; i < BOARD_SIZE; i++) {
+        for (let j = 0; j < BOARD_SIZE; j++) {
+            board[i][j] = boardCopy[i][j];
+        }
+    }
     return changed;
 }
+
 
 function updateActive(board, active) {
     for (let i = 0; i < BOARD_SIZE; i++) {
@@ -172,24 +235,36 @@ function updateActive(board, active) {
     }
 }
 
-function fall(board, active) {
+
+function fall(board, active, shift) {
     for (let i = 0; i < BOARD_SIZE; i++) {
-        for (let j = BOARD_SIZE - 1; j > 0; j--) {
+        for (let j = BOARD_SIZE - 1; j >= 0; j--) {
             if (!active[i][j]) {
                 // Обмен значений между соседними ячейками
-                [board[i][j], board[i][j - 1]] = [board[i][j - 1], board[i][j]];
+                shift[i][j] += 1;
+                if (shift[i][j] == FALL_SLOWNESS) {
+                    if (j > 0) {
+                        [board[i][j], board[i][j - 1]] = [board[i][j - 1], board[i][j]];
+                    }
+                    shift[i][j] = 0;
+                    //shift[i][j - 1] = -FALL_SLOWNESS + 1;
+                }
+            } else {
+                shift[i][j] = 0;
             }
         }
         // Замена верхнего элемента новым случайным значением, если он неактивен
-        if (!active[i][0]) {
+        if (!active[i][0] && shift[i][0] == 0) {
             board[i][0] = Math.floor(Math.random() * circleColors.length);
         }
     }
 }
 
+
 function checkSwap(a, b) {
     return Math.abs(a.x - b.x) + Math.abs(a.y - b.y) === 1;
 }
+
 
 function resizeCanvas() {
     var canvas = document.getElementById('gameCanvas');
@@ -213,9 +288,11 @@ function main() {
     // Игровое поле
     let board = [];
     let active = [];
+    let shift = [];
     for (let i = 0; i < BOARD_SIZE; i++) {
         board.push(new Array(BOARD_SIZE).fill(0));
         active.push(new Array(BOARD_SIZE).fill(true));
+        shift.push(new Array(BOARD_SIZE).fill(0));
     }
 
     changeColors(board);
@@ -284,18 +361,17 @@ function main() {
         // Отрисовка доски
         match3(board, active);
         updateActive(board, active);
-        if (iteration % 10 == 0) {
-            fall(board, active);
-            updateActive(board, active);
-        }
-        drawBoard(ctx, board, pos, selected, active);
+        fall(board, active, shift);
+        updateActive(board, active);
+        drawBoard(ctx, board, pos, selected, active, shift);
 
         // Запрос на следующее обновление
-        requestAnimationFrame(update);
+        // requestAnimationFrame(update);
     }
 
     // Начало цикла обновления
     requestAnimationFrame(update);
+    setInterval(update, ANIMATION_INTERVAL);
 }
 
 // Запуск игры
